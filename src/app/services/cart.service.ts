6cb4f8872e0f8204b8ed3490c4cc7308f65d95db
mitra47 <73024958+mitra47@users.noreply.github.com>
2021-03-18 00:16:52 +0300

    18.03.2021 ders sonu

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CarItem } from '../models/cartItem';
+import { CartItems } from '../models/cartItems';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+  addToCart(product:Product){
+    let item =CartItems.find(c=>c.product.productId===product.productId);
+    if(item){
+      item.quantity+=1;
+    }else{
+      let carItem = new CarItem();
+      carItem.product=product;
+      carItem.quantity=1;
+      
+      CartItems.push(carItem)
+    }
+  }
+  removeFromCart(product:Product){
+    let item:CarItem =CartItems.find(c=>c.product.productId===product.productId);
+    CartItems.splice(CartItems.indexOf(item),1);
+  }
+  list():CarItem[]{
+    return CartItems;
+  }
+}
